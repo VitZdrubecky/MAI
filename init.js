83b8e6016bc5fea801510985d9d4c2934441f38b
Martin Troup <troupmar@gmail.com>
2015-03-25 12:54:53 +0100

    Added right-to-left moorhuhn. TODO -> need to fix coordinates!

--- init.js
+++ init.js
@@ -30,8 +30,8 @@ window.onload = function()
     ]);
     queue.load();
     
-        // Create bat spritesheet
-    var spriteConfig = 
+    // Create bat spritesheet
+    var spriteFlyRightConfig = 
         {
             "images": ["./assets/fly_sprint_left_to_right.svg"],
             "frames": [
@@ -50,20 +50,56 @@ window.onload = function()
                 [1824, 2, 150, 140], 
             ],
             "animations": {
-                "flap": [0, 12], 
+                "flapRight": [0, 12], 
             },
         }
-    spriteSheet = new createjs.SpriteSheet(spriteConfig);
+
+    spriteFlyRight = new createjs.SpriteSheet(spriteFlyRightConfig);
     
-    animation = new createjs.Sprite(spriteSheet, "flap");
-    animation.regX = 99;
-    animation.regY = 58;
-    animation.x = 200;
-    animation.y = 200;
-    animation.gotoAndPlay("flap");
-    stage.addChildAt(animation);
+    animFlyRight = new createjs.Sprite(spriteFlyRight, "flap");
+    animFlyRight.regX = 99;
+    animFlyRight.regY = 58;
+    animFlyRight.x = 200;
+    animFlyRight.y = 200;
+    animFlyRight.gotoAndPlay("flapRight");
+    stage.addChildAt(animFlyRight);
+
+    // Create bat spritesheet
+    var spriteFlyLeftConfig = 
+        {
+            "images": ["./assets/fly_sprint_right_to_left.svg"],
+            "frames": [
+                [2, 2, 135, 143], 
+                [139, 2, 135, 143],
+                [276, 2, 133, 143], 
+                [411, 2, 136, 140], 
+                [549, 2, 145, 140], 
+                [696, 2, 148, 140], 
+                [846, 2, 160, 140],
+                [1008, 2, 163, 140],
+                [1173, 2, 168, 140], 
+                [1340, 2, 163, 140], 
+                [1505, 2, 163, 140], 
+                [1670, 2, 155, 140], 
+                [1824, 2, 150, 140],  
+            ],
+            "animations": {
+                "flapLeft": [0, 12], 
+            },
+        }
+
+    spriteFlyLeft = new createjs.SpriteSheet(spriteFlyLeftConfig);
+    
+    animFlyLeft = new createjs.Sprite(spriteFlyLeft, "flap");
+    animFlyLeft.regX = 99;
+    animFlyLeft.regY = 58;
+    animFlyLeft.x = 400;
+    animFlyLeft.y = 200;
+    animFlyLeft.gotoAndPlay("flapLeft");
+    stage.addChildAt(animFlyLeft);
 
     
+
     // Add ticker
     createjs.Ticker.setFPS(15);
     createjs.Ticker.addEventListener('tick', stage);
