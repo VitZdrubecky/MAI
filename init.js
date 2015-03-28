986d4bd457b9417a4f63730940c529d442ed3adc
Martin Troup <troupmar@gmail.com>
2015-03-28 18:53:53 +0100

    Aim target added. It reacts on mouse moves.

--- init.js
+++ init.js
@@ -4,6 +4,10 @@ var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 var stage;
 var image;
+var target;
+
+//var moorhuhnX;
+//var moorhuhnY;
 
 $(document).ready(function() { 
     
@@ -29,6 +33,25 @@ $(document).ready(function() {
 
 });
 
+function queueLoaded(event) {
+    // Loading background image
+    backgroundImage = new Image();
+    backgroundImage.src = "./assets/background/playground-moorhuhn.svg";
+    backgroundImage.onload = handleBackgroundImageLoad;
+
+    //Loading animations
+    createAnimation(getFlyRightSpriteConfig(), 200, 200, 1.0, 1.0, "flapLeft");
+    createAnimation(getFlyLeftSpriteConfig(), 400, 200, 0.8, 0.8, "flapRight");
+    createAnimation(getKillSpriteConfig(), 650, 200, 1.1, 1.1, "kill");
+
+    targetImage = new Image();
+    targetImage.src = "./assets/target.svg";
+    targetImage.onload = handleTargetImageLoad;
+    
+    window.onmousemove = handleMouseMove;
+
+}
+
 function createAnimation(spriteConfig, x, y, scaleX, scaleY, animation) {
     // Create sprite
     sprite = new createjs.SpriteSheet(spriteConfig);
@@ -42,25 +65,27 @@ function createAnimation(spriteConfig, x, y, scaleX, scaleY, animation) {
     stage.addChildAt(animation);
 }
 
-function queueLoaded(event) {
-    // Loading background image
-    image = new Image();
-    image.src = "./assets/background/playground-moorhuhn.svg";
-    image.onload = handleBackgroundImageLoad;
-
-    //Loading animations
-    createAnimation(getFlyRightSpriteConfig(), 200, 200, 1.0, 1.0, "flapLeft");
-    createAnimation(getFlyLeftSpriteConfig(), 400, 200, 0.8, 0.8, "flapRight");
-    createAnimation(getKillSpriteConfig(), 650, 200, 1.1, 1.1, "kill");
-    
-
-}
-
 function handleBackgroundImageLoad() {
-    var backgroundImage = new createjs.Bitmap(image);
+    var background = new createjs.Bitmap(backgroundImage);
     //backgroundImage.scaleX = 1.0;
     //backgroundImage.scaleY = 1.0;
-    backgroundImage.set({alpha: 0.85});
-    backgroundImage.cache(0, 0, WIDTH, HEIGHT /*[, possibleScale]*/);
-    stage.addChildAt(backgroundImage);
+    background.set({alpha: 0.85});
+    background.cache(0, 0, WIDTH, HEIGHT /*[, possibleScale]*/);
+    stage.addChildAt(background, 0);
+}
+
+function handleTargetImageLoad() {
+    target = new createjs.Bitmap(targetImage);
+    target.x = WIDTH/2;
+    target.y = HEIGHT/2;
+    target.scaleX = 0.15;
+    target.scaleY = 0.15;
+    stage.addChildAt(target, stage.getNumChildren());
+}
+
+function handleMouseMove(event)
+{
+    //Offset the position by 45 pixels so mouse is in center of crosshair
+    target.x = event.clientX-45;
+    target.y = event.clientY-45;
 }
